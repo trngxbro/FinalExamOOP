d138b1414c30fcaf1015a0e35de0f5638f33a0f6
trngxbro <84927898+trngxbro@users.noreply.github.com>
2021-07-10 19:03:42 +0700

    Add files via upload

---
+++ TestSV.java
@@ -0,0 +1,11 @@
+public class Test {
+	public static void main (String [] args) {
+		SinhVien sv  = new SinhVien ("Nguyen Trung Nghia", "52000693");
+		SinhVien svn = new SinhVien ("Nguyen Trung Nghia", "52000693", "28/05/02");
+		System.out.println(sv);
+		System.out.println(svn);
+	}
+}
+
+
+
